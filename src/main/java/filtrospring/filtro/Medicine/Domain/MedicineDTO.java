0c50ca34a2efe282229214d723c83be442d28d6e
a <>
2024-08-13 09:07:12 -0500

    Desarrollo dto.

---
+++ src/main/java/filtrospring/filtro/Medicine/Domain/MedicineDTO.java
@@ -0,0 +1,20 @@
+package filtrospring.filtro.Medicine.Domain;
+
+public class MedicineDTO {
+    
+}
+
+
+/*
+{
+    "description" : "Ganancia de peso con baja dieta es la solucion",
+    "description_short" : "Aumentador de peso",
+    "health_register": "health_register",
+    "name_medicine" : "Mass gainer",
+    "name_rol": "admin",
+    "proceedings": "procedencia conocida",
+    "code_ap_id" : 1, 
+    "code_lab_id" : 1,
+    "code_mode_admin_id" : 1,
+    "code_um_id" : 1
+} */
